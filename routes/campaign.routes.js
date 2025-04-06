bab0bbbbd2f8d718e16233184a2aa62fa0a5b8c7
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-04-06 08:39:32 +0530

    Added campaign routes, controllers and models

--- routes/campaign.routes.js
+++ routes/campaign.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const campaignRouter = Router()
 
-import { getAllCampaigns, getCampaign, generateCampaign, deleteCampaign } from "../controllers/campaign.controller";
+import { getAllCampaigns, getCampaign, generateCampaign, deleteCampaign } from "../controllers/campaign.controller.js";
 
 campaignRouter.get("/", getAllCampaigns)
 
